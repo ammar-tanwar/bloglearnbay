0b13230d0c8b36ae68fd67d4ce4f864f940099d4
Mohd-w123 <codewithwazid@gmail.com>
2022-11-21 15:11:36 +0530

    21 nov post

--- pages/[id].js
+++ pages/[id].js
@@ -33,6 +33,7 @@ export default function Post({ postData, posts }) {
         },
         "title": "${postData.title}",
         "image": "${postData.img}", 
+        "tag": "${postData.tag}", 
         "author": {
           "@type": "Person",
           "author": "${postData.author}"
