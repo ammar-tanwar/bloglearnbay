de7dabc3ec268dfe402df21bfd2f082a90cb89fe
Mohd-w123 <codewithwazid@gmail.com>
2023-01-13 12:42:09 +0530

    13 jan post

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,7 +4,7 @@ const posts = process.env.NODE_ENV === 'production' ? require('../../cache/data.
 
 export default (req, res) => {
   const results = req.query.q ?
-    posts.filter(post => post.title.toLowercase().includes(req.query.q)) : []
+      posts.filter(post => post.title.toLowerCase().includes(req.query.q)) : []
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({ results }))
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,7 @@
 export const sortByDate = (a, b) => {
   return new Date(b.date) - new Date(a.date);
 };
+
+export const sortByDateEvent = (a, b) => {
+  return new Date(b.eventDate) - new Date(a.eventDate);
+};
