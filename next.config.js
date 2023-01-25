3cd0d437ebfc189d03af415f5c08ecf6f9918035
Mohd-w123 <codewithwazid@gmail.com>
2023-01-25 13:24:38 +0530

    25 jan post

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["learnbay-wb.s3.ap-south-1.amazonaws.com"],
   },
+
+ 
+
 
   async redirects() {
     return [
