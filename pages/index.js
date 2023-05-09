662aba05810eb2813734aa58ab3abd8a1a464c74
Mohd-w123 <codewithwazid@gmail.com>
2023-05-09 17:55:15 +0530

    lang tag added

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,6 @@ export default function blog({ allPostsData }) {
         <meta name="google-site-verification" content="q2xA2OZrvhAj8r1YGNF_3x5m5GuWCqo9rNb7atG4mXU" />
         <link rel="icon" href="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/Learnbay-Favicon-L.png" />
         <link rel="canonical" href="https://blog.learnbay.co/" />
-        {/* <meta name="author" content="Learnbay" /> */}
         <meta name="publisher" content="Learnbay" />
         <meta name="keywords" content="Learnbay Blogs" />
         <meta name="robots" content="index, follow"/>
